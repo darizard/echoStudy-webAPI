758c4cde2afe7a7ef136d0b45e27d187fa57abb3
Jon Strode <jstrode84@gmail.com>
2022-04-17 20:17:41 -0600

    Implement Deck edit endpoint

--- Controllers/DecksController.cs
+++ Controllers/DecksController.cs
@@ -235,28 +235,41 @@ namespace echoStudy_webAPI.Controllers
         }
         */
 
-        // POST: api/Decks/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        // POST: /Decks/{id}
         /// <summary>
-        /// Edits a Deck ... (UNIMPLEMENTED)
+        /// Edits an existing Deck
         /// </summary>
-        /// <param name="userId">The ASP.NET Id of the related user. Overrides <c>userEmail</c> if present</param>
-        /// <param name="userEmail">The email address of the related user</param>
+        /// <remarks>
+        /// Currently does not support changing its associated cards
+        /// </remarks>
+        /// <param name="id">ID of the Deck to edit</param>
+        /// <param name="deckInfo">
+        /// Optional: title, description, default_flang, default_blang, userId, access, cardIds
+        /// </param>
         /// <returns>A JSON list of Deck objects</returns>
         [HttpPost("{id}")]
         public async Task<IActionResult> PostDeckEdit(int id, PostDeckInfo deckInfo)
         {
+            Deck deck;
             var deckQuery = from d in _context.Decks
                             where d.DeckID == id
                             select d;
-            // Create the deck
-            if (!deckQuery.Any())
-            {
-                // Create and populate a deck with the given info
-                Deck deck = new Deck();
+            if((deck = deckQuery.First()) is null) return NotFound("Deck id " + id + " not found");
+
+            if (deckInfo.title is not null)
+            { 
+                if (deckInfo.title == "") return BadRequest("title cannot be empty");
                 deck.Title = deckInfo.title;
+            }
+
+            if (deckInfo.description is not null)
+            {
+                if (deckInfo.description == "") return BadRequest("description cannot be empty");
                 deck.Description = deckInfo.description;
-                // Handle the enums with switch cases
+            }
+
+            if (deckInfo.access is not null)
+            {
                 switch (deckInfo.access.ToLower())
                 {
                     case "public":
@@ -268,6 +281,10 @@ namespace echoStudy_webAPI.Controllers
                     default:
                         return BadRequest("Valid access parameters are Public and Private");
                 }
+            }
+            
+            if (deckInfo.default_flang is not null)
+            {
                 switch (deckInfo.default_flang.ToLower())
                 {
                     case "english":
@@ -285,113 +302,10 @@ namespace echoStudy_webAPI.Controllers
                     default:
                         return BadRequest("Current supported languages are English, Spanish, Japanese, and German");
                 }
-                switch (deckInfo.default_blang.ToLower())
-                {
-                    case "english":
-                        deck.DefaultBackLang = Language.English;
-                        break;
-                    case "spanish":
-                        deck.DefaultBackLang = Language.Spanish;
-                        break;
-                    case "japanese":
-                        deck.DefaultBackLang = Language.Japanese;
-                        break;
-                    case "german":
-                        deck.DefaultBackLang = Language.German;
-                        break;
-                    default:
-                        return BadRequest("Current supported languages are English, Spanish, Japanese, and German");
-                }
-                // Associate the deck with the appropriate cards
-                deck.Cards = new List<Card>();
-                foreach (int cardId in deckInfo.cardIds)
-                {
-                    // Grab the deck. Only possible for one or zero results since ids are unique.
-                    var query = from c in _context.Cards
-                                where c.CardID == cardId
-                                select c;
-                    if (!query.Any())
-                    {
-                        return BadRequest("Deck id " + cardId + " does not exist");
-                    }
-                    else
-                    {
-                        Card card = query.First();
-                        card.Deck = deck;
-                        deck.Cards.Add(card);
-                        _context.Entry(card).State = EntityState.Modified;
-                    }
-                }
-                // Owner
-                EchoUser user = await _userManager.FindByEmailAsync(deckInfo.userEmail);
-                if (user is null)
-                {
-                    return BadRequest("User " + deckInfo.userEmail + " not found");
-                }
-                else
-                {
-                    deck.UserId = user.Id;
-                }
-                // Dates
-                deck.DateCreated = DateTime.Now;
-                deck.DateTouched = DateTime.Now;
-                deck.DateUpdated = DateTime.Now;
-
-                // Save the deck
-                _context.Decks.Add(deck);
-                await _context.SaveChangesAsync();
-
-                return CreatedAtAction("GetDeck", new { id = deck.DeckID }, deck);
             }
-            // Update the deck
-            else
+
+            if (deckInfo.default_blang is not null)
             {
-                Deck deck = deckQuery.First();
-
-                // Assign the owner
-                EchoUser user = await _userManager.FindByEmailAsync(deckInfo.userEmail);
-                if (user is null)
-                {
-                    return BadRequest("User " + deckInfo.userEmail + " not found");
-                }
-                else
-                {
-                    deck.UserId = user.Id;
-                }
-
-                // Set description, title
-                deck.Title = deckInfo.title;
-                deck.Description = deckInfo.description;
-
-                // Set the enums
-                switch (deckInfo.access.ToLower())
-                {
-                    case "public":
-                        deck.Access = Access.Public;
-                        break;
-                    case "private":
-                        deck.Access = Access.Private;
-                        break;
-                    default:
-                        return BadRequest("Valid access parameters are Public and Private");
-                }
-                switch (deckInfo.default_flang.ToLower())
-                {
-                    case "english":
-                        deck.DefaultFrontLang = Language.English;
-                        break;
-                    case "spanish":
-                        deck.DefaultFrontLang = Language.Spanish;
-                        break;
-                    case "japanese":
-                        deck.DefaultFrontLang = Language.Japanese;
-                        break;
-                    case "german":
-                        deck.DefaultFrontLang = Language.German;
-                        break;
-                    default:
-                        return BadRequest("Current supported languages are English, Spanish, Japanese, and German");
-                }
                 switch (deckInfo.default_blang.ToLower())
                 {
                     case "english":
@@ -409,62 +323,36 @@ namespace echoStudy_webAPI.Controllers
                     default:
                         return BadRequest("Current supported languages are English, Spanish, Japanese, and German");
                 }
-
-                // Assign it to the cards given by id (if there is any)
-                HashSet<Card> updatedCards = new HashSet<Card>();
-                foreach (int cardId in deckInfo.cardIds)
-                {
-                    // Grab the deck. Only possible for one or zero results since ids are unique.
-                    var query = from c in _context.Cards
-                                where c.CardID == cardId
-                                select c;
-                    if (query.Count() == 0)
-                    {
-                        return BadRequest("Deck id " + cardId + " does not exist");
-                    }
-                    else
-                    {
-                        Card card = query.First();
-                        // If they aren't already related, relate them
-                        if (!deck.Cards.Contains(card))
-                        {
-                            card.Deck = deck;
-                            deck.Cards.Add(card);
-                        }
-                        updatedCards.Add(card);
-                        _context.Entry(card).State = EntityState.Modified;
-                    }
-                }
-
-                // Change update date
-                deck.DateUpdated = DateTime.Now;
-
-                // Mark the deck as modified
-                _context.Entry(deck).State = EntityState.Modified;
-
-                try
-                {
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    return BadRequest("Failed to update card");
-                }
-
-                return Ok(new { message = "Card was successfully updated.", deck });
             }
+
+            // Owner
+            if(deckInfo.userId is not null)
+            {
+                EchoUser user = await _userManager.FindByIdAsync(deckInfo.userId);
+                if (user is null) return BadRequest("User " + deckInfo.userId + " not found");
+
+                deck.UserId = user.Id;
+            }
+
+            // Dates
+            deck.DateUpdated = DateTime.Now;
+
+            // Save the deck
+            _context.Decks.Update(deck);
+            await _context.SaveChangesAsync();
+
+            return Ok();
         }
 
         // POST: api/Decks
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         /// <summary>
         /// Creates a Deck for a specific user
         /// </summary>
         /// <param name="deckInfo">
-        /// Required: title, description, default_flang, default_blang, userEmail -- Optional: access, cardIds
+        /// Required: title, description, default_flang, default_blang, userId -- Optional: access, cardIds
         /// </param>
         /// <remarks>Default access level is Private. The cardIds list currently does nothing.</remarks>
-        /// <returns>A JSON list of Deck objects</returns>
+        /// <returns>The id and creation date of the new deck</returns>
         [HttpPost]
         public async Task<ActionResult<Deck>> PostDeckCreate(PostDeckInfo deckInfo)
         {
@@ -570,37 +458,11 @@ namespace echoStudy_webAPI.Controllers
             await _context.Decks.AddAsync(deck);
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction("PostDeckCreate", new { id = deck.DeckID }, deck);
-
-            // Associate the listed cards to the new deck
-            foreach (Card card in querycards) { card.DeckID = deck.DeckID; }
-            await _context.SaveChangesAsync();
-
-            foreach (int cardId in deckInfo.cardIds)
+            return CreatedAtAction("PostDeckCreate", new
             {
-                // Grab the deck. Only possible for one or zero results since ids are unique.
-                var queryold = from c in _context.Cards
-                            where c.CardID == cardId
-                            select c;
-                if (!queryold.Any())
-                {
-                    return BadRequest("Card id " + cardId + " does not exist");
-                }
-                else
-                {
-                    Card card = queryold.First();
-                    card.CardID = deck.DeckID;
-                    deck.Cards.Add(card);
-                    _context.Entry(card).State = EntityState.Modified;
-                }
-            }
-
-
-            // Save the deck
-            _context.Decks.Add(deck);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetDeck", new { id = deck.DeckID }, deck);
+                id = deck.DeckID,
+                dateCreated = deck.DateCreated
+            });
         }
 
         /*
