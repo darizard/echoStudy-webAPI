9f7717ce7ee15d09da8a0279f12a88e082dd5db3
masonitea <masoniteg@gmail.com>
2022-09-15 00:44:03 -0600

    Added helperfunctions and models
    Promotes better organization

---
+++ HelperFunctions.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using static echoStudy_webAPI.Tests.Models.Models;
+
+namespace echoStudy_webAPI.Tests
+{
+    internal static class HelperFunctions
+    {
+
+        /**
+        * Gets John's token, should not be empty
+         */
+        public static async Task GrabJohnToken(HttpClient client)
+        {
+            // The user
+            UserInfo userDetails = new UserInfo();
+            userDetails.username = "JohnDoe@gmail.com";
+            userDetails.password = "123ABC!@#def";
+
+            // Get the token from the server
+            HttpContent signInContent = new StringContent(JsonConvert.SerializeObject(userDetails), Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await client.PostAsync("authenticate", signInContent);
+
+            // Parse and set the authorization header in the client
+            if (response.IsSuccessStatusCode)
+            {
+                authResponse token = JsonConvert.DeserializeObject<authResponse>(await response.Content.ReadAsStringAsync());
+
+                client.DefaultRequestHeaders.Authorization =
+    new AuthenticationHeaderValue("Bearer", token.Token);
+                return;
+            }
+
+            throw new Exception("Couldn't obtain John's JWT Token");
+        }
+        /**
+        * Grabs the user ID from the JWT token
+        */
+        public static async Task<string> GrabUserId(HttpClient client)
+        {
+            // TEST 1: Get a card that belongs to John
+            HttpResponseMessage response = await client.GetAsync("Cards/1");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var contents = await response.Content.ReadAsStringAsync();
+                CardInfo card = JsonConvert.DeserializeObject<CardInfo>(contents);
+                return card.ownerId;
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+    }
+}
---
+++ Models.cs
@@ -0,0 +1,114 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace echoStudy_webAPI.Tests.Models
+{
+    internal static class Models
+    {
+        public class authResponse
+        {
+            public string RefreshToken { get; set; }
+            public string Token { get; set; }
+        }
+
+
+        /**
+         * User Info
+         */
+        public class UserInfo
+        {
+            public string username { get; set; }
+            public string password { get; set; }
+        }
+
+        /**
+         * Response from API
+         */
+        public class CreatedResponse
+        {
+            public int id { get; set; }
+            public DateTime dateCreated { get; set; }
+        }
+
+        /**
+         * Response from API
+         */
+        public class UpdatedResponse
+        {
+            public int id { get; set; }
+            public DateTime DateUpdated { get; set; }
+        }
+
+        /**
+         * This class should contain all information that should be returned to the user in a GET request
+         */
+        public class CardInfo
+        {
+            public int id { get; set; }
+            public string ftext { get; set; }
+            public string btext { get; set; }
+            public string faud { get; set; }
+            public string baud { get; set; }
+            public string flang { get; set; }
+            public string blang { get; set; }
+            public int deckId { get; set; }
+            public int score { get; set; }
+            public string ownerId { get; set; }
+            public DateTime date_created { get; set; }
+            public DateTime date_updated { get; set; }
+            public DateTime date_touched { get; set; }
+        }
+
+        /**
+         * This class should contain all information that should be provided in order to create or update a card
+         */
+        public class PostCardInfo
+        {
+            public string frontText { get; set; }
+            public string backText { get; set; }
+            public string frontLang { get; set; }
+            public string backLang { get; set; }
+            public string userId { get; set; }
+            public int? deckId { get; set; }
+        }
+
+
+        /**
+        * This class should contain all information needed in a GET request
+        */
+        public class DeckInfo
+        {
+            public int id { get; set; }
+            public string title { get; set; }
+            public string description { get; set; }
+            public string access { get; set; }
+            public string default_flang { get; set; }
+            public string default_blang { get; set; }
+            public string ownerId { get; set; }
+            public List<int> cards { get; set; }
+            public DateTime date_created { get; set; }
+            public DateTime date_updated { get; set; }
+            public DateTime date_touched { get; set; }
+        }
+
+        /**
+         * This class should contain all information needed from the user to create a row in the database
+         */
+        public class PostDeckInfo
+        {
+            public string title { get; set; }
+            public string description { get; set; }
+            public string access { get; set; }
+            public string default_flang { get; set; }
+            public string default_blang { get; set; }
+            public string userId { get; set; }
+            public List<int> cardIds { get; set; }
+        }
+    }
+}
