c4364beb415963c1e0e153894bca935a2a547f9e
masonitea <masoniteg@gmail.com>
2022-10-24 11:34:15 -0600

    Added DateCreated to users

--- Areas/Identity/Data/EchoUser.cs
+++ Areas/Identity/Data/EchoUser.cs
@@ -1,10 +1,26 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
 
 namespace echoStudy_webAPI.Areas.Identity.Data
 {
     public class EchoUser : IdentityUser
     {
-        
+        public DateTime? DateCreated
+        {
+            /**
+             * Grabs the creation date or sets it if it does not exist
+             */
+            get
+            {
+                if(DateCreated == null)
+                {
+                    DateCreated = DateTime.Now;
+                }
+                return DateCreated;
+            }
+            private set {}
+        }
     }
 }
---
+++ Migrations/EchoStudyDBMigrations/20221024173351_AddUserDateCreated.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace echoStudy_webAPI.Migrations.EchoStudyDBMigrations
+{
+    public partial class AddUserDateCreated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreated",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                table: "AspNetUsers");
+        }
+    }
+}
