d01df2c2ca0cb8fa52bfe93594015398df9a5aa9
masonitea <masoniteg@gmail.com>
2022-11-02 18:33:13 -0600

    Added migration for card's custom audio fields

--- Migrations/EchoStudyDBMigrations/20221103003252_AddCustomAudio.Designer.cs
+++ Migrations/EchoStudyDBMigrations/20221103003252_AddCustomAudio.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using echoStudy_webAPI.Models;
 
 namespace echoStudy_webAPI.Migrations.EchoStudyDBMigrations
 {
     [DbContext(typeof(EchoStudyDB))]
-    partial class EchoStudyDBModelSnapshot : ModelSnapshot
+    [Migration("20221103003252_AddCustomAudio")]
+    partial class AddCustomAudio
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/EchoStudyDBMigrations/20221103003252_AddCustomAudio.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace echoStudy_webAPI.Migrations.EchoStudyDBMigrations
+{
+    public partial class AddCustomAudio : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CustomBackAudio",
+                table: "Cards",
+                type: "nvarchar(4000)",
+                maxLength: 4000,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CustomFrontAudio",
+                table: "Cards",
+                type: "nvarchar(4000)",
+                maxLength: 4000,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomBackAudio",
+                table: "Cards");
+
+            migrationBuilder.DropColumn(
+                name: "CustomFrontAudio",
+                table: "Cards");
+        }
+    }
+}
